1643830177c65fa09d7d1f600afeada002b938c2
Rizwan Khan <rizashifa845454@gmail.com>
2023-09-27 17:39:33 +0530

    project completed

---
+++ script.js
@@ -0,0 +1,67 @@
+let center = document.querySelector("#center");
+
+center.addEventListener("mousemove", function (dets) {
+  let recLocationVal = center.getBoundingClientRect();
+  let insideRect = dets.clientX - recLocationVal.left;
+
+  if (insideRect < recLocationVal.width / 2) {
+    let redColor = gsap.utils.mapRange(
+      0,
+      recLocationVal.width / 2,
+      255,
+      0,
+      insideRect
+    );
+    gsap.to(center, {
+      backgroundColor: `rgb(${redColor},0, 0)`,
+    });
+  } else {
+    let blueColor = gsap.utils.mapRange(
+        recLocationVal.width / 2,
+        recLocationVal.width,
+        0,
+        255,
+        insideRect
+      );
+      gsap.to(center, {
+        backgroundColor: `rgb(0, 0,${blueColor})`,
+        ease: Power4
+      });
+  }
+});
+center.addEventListener("mousemove", function (dets) {
+  let recLocationVal = center.getBoundingClientRect();
+  let insideRect = dets.clientX - recLocationVal.left;
+
+  if (insideRect < recLocationVal.width / 2) {
+    let redColor = gsap.utils.mapRange(
+      0,
+      recLocationVal.width / 2,
+      255,
+      0,
+      insideRect
+    );
+    gsap.to(center, {
+      backgroundColor: `rgb(${redColor},0, 0)`,
+    });
+  } else {
+    let blueColor = gsap.utils.mapRange(
+        recLocationVal.width / 2,
+        recLocationVal.width,
+        0,
+        255,
+        insideRect
+      );
+      gsap.to(center, {
+        backgroundColor: `rgb(0, 0,${blueColor})`,
+        ease: Power4
+      });
+  }
+});
+
+center.addEventListener("mouseleave", function (){
+    gsap.to(center, {
+        backgroundColor: "#fff",
+        ease: Power4
+      });
+})
